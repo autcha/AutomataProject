ea1689124b379713a5b37e5e5f7fc81b36d3f65f
Autumn Chavez <amchavez088@gmail.com>
2019-07-13 15:19:28 -0400

    Basic Template
    - Inlcudes main page & the language page
    - Also has js file for the electron setup

---
+++ main.js
@@ -0,0 +1,33 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+let win;
+
+function createWindow(){
+  win = new BrowserWindow({
+    width: 1000,
+    height: 1000,
+    webPreferences:{
+      nodeIntegration: true
+    }
+  });
+  //win.loadFile(__dirname + 'index.html');
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+  win.on('closed', function(){
+    win = null;
+  });
+}
+
+  app.on('ready', createWindow);
+
+  app.on('window-all-closed', function(){
+    if(process.platform !== 'darwin') app.quit();
+  })
+
+  app.on('activate', function(){
+    if(win === null) createWindow();
+  })
