4e115bc1f66306baa95c288ca5a1599b9e55af96
dubliyu <cleonromero@gmail.com>
2019-07-14 18:49:14 -0400

    color coded dna encoding

--- scripts/shapiro.js
+++ scripts/shapiro.js
@@ -9,7 +9,9 @@ let state = "TRANSITION"
 let input_w = "";
 let input_w_x  = 0;
 let input_w_y = 0;
-const SIZE = 25;
+let bottom_row = ""
+let top_row = "";
+let SIZE = 25;
 
 
 function start_automaton(){
@@ -72,7 +74,7 @@ function show_splitting_canvas(){
 function draw_start_cutting(){
 	// Set the header
 	let h4 = document.getElementsByClassName("comp_desc")[0];
-	h4.innerText = `.`;
+	h4.innerText = `Transactions and FokI cutting demonstrated below.`;
 	
 	// Draw the cutting action
 	init_cutting();
@@ -84,8 +86,11 @@ function draw_start_cutting(){
 
 function init_cutting(){
 	// Show the input string
+	let canvas = document.getElementById("two_state_3");
+	let ctx = canvas.getContext("2d");
+	draw_just_input_string(ctx, top_row, bottom_row);
 
-	// Show the transitions on the left
+	// Show the transitions on top
 }
 
 function start_cutting(){
@@ -106,8 +111,8 @@ function show_encode_canvas(){
 function draw_encoded_string(){
 	// Draw the series of boxes representing a string in DNA form
 	let input_string = input_w;
-	let top_row = "";
-	let bottom_row = "";
+	top_row = "";
+	bottom_row = "";
 	let spacer_top = "GCC";
 	let spacer_bottom = "CGG";
 	let terminator_top = "GTCGG";
@@ -142,30 +147,67 @@ function draw_encoded_string(){
 	// Draw them on the canvas
 	let canvas = document.getElementById("two_state_2");
 	let ctx = canvas.getContext("2d");
+	draw_just_input_string(ctx, top_row, bottom_row);
+
+}
+
+function draw_just_input_string(ctx, top_row, bottom_row){
 	ctx.strokeStyle = 'salmon';
 	ctx.lineWidth = 2;
-
+	SIZE = (top_row.length > 30 ? 15 : 25);
 	let x = input_w_x;
 	let y = input_w_y;
+	let count = -4;
+	let draw_state = 0;
 	for(let w of top_row){
-		draw_square(ctx, x, y, w);
+		if(count % 8 == 0 || count % 8 == 1 || count % 8 == 2){
+			draw_state = 0;
+		}
+		else if(count + 5 >= top_row.length - 5){
+			draw_state = 1;
+		}
+		else{
+			draw_state = 2;
+		}
+		draw_square(ctx, x, y, w, draw_state);
 		x += SIZE;
+		count++;
 	}
 
 	x = input_w_x + (SIZE * 4);
 	y = input_w_y + SIZE;
+	count = 0;
 	for(let w of bottom_row){
-		draw_square(ctx, x, y, w);
+		if(count % 8 == 0 || count % 8 == 1 || count % 8 == 2){
+			draw_state = 0;
+		}
+		else if(count >= bottom_row.length - 5){
+			draw_state = 1;
+		}
+		else{
+			draw_state = 2;
+		}
+		draw_square(ctx, x, y, w, draw_state);
 		x += SIZE;
+		count++;
 	}
-
 }
 
-function draw_square(ctx, x, y, letter){
+function draw_square(ctx, x, y, letter, state){
+	if(state === undefined){
+		ctx.fillStyle = "#7cff4d";
+	}else{
+		switch(state){
+			case 0: ctx.fillStyle = "#1d1f1e"; break;
+			case 1: ctx.fillStyle = "#b45fc9"; break;
+			case 2: ctx.fillStyle = "#7cff4d"; break;
+			default:
+			 	ctx.fillStyle = "#7cff4d";
+		}
+	}
 	ctx.strokeRect(x, y, SIZE, SIZE);
-	ctx.font = "20px 'Courier New', Courier, monospace";
-	ctx.fillStyle = '#7cff4d';
-	ctx.fillText(letter, x + 5, y + 20);
+	ctx.font = `${SIZE - 5}px 'Courier New', Courier, monospace`;
+	ctx.fillText(letter, x + (SIZE / 6), y + SIZE - 5);
 }
 
 function draw_transition_encoding(){
